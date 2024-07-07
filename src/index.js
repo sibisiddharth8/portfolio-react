f23e13a27cbd09f4ebcbe92ba8b964ef1ef68092
Sibi Siddharth S <sibisiddharth008@gmail.com>
2024-07-07 23:05:27 +0530

    Update routing and homepage for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
-
