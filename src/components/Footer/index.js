8cd5242f7f7dee43c715c5342d61c414ccc4e878
Sibi Siddharth S <sibisiddharth008@gmail.com>
2024-09-01 12:31:17 +0530

    perfomance optimization

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -107,14 +107,6 @@ const Footer = ({ footerData }) => {
   const handleInstallClick = () => {
     if (deferredPrompt) {
       deferredPrompt.prompt();
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        setDeferredPrompt(null);
-      });
     }
   };
 
