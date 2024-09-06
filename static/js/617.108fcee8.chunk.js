cf83413bc2e75da7c231293f3d449c79002ce00a
Sibi Siddharth S <sibisiddharth008@gmail.com>
2024-09-06 19:09:37 +0530

    Updates

--- static/js/617.108fcee8.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkportfolio_react=self.webpackChunkportfolio_react||[]).push([[617],{8617:function(t,r,o){o.d(r,{Hi9:function(){return n}});var e=o(9983);function n(t){return(0,e.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"}}]})(t)}}}]);
-//# sourceMappingURL=617.108fcee8.chunk.js.map
