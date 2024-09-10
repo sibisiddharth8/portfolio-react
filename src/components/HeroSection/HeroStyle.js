dd53aa28b4ef90258f9f7f8f2c4142e2c5737cca
Sibi Siddharth S <sibisiddharth008@gmail.com>
2024-09-10 17:48:56 +0530

    Hero Section dvh

--- src/components/HeroSection/HeroStyle.js
+++ src/components/HeroSection/HeroStyle.js
@@ -7,6 +7,9 @@ export const HeroContainer = styled.div`
   justify-content: center;
   position: relative;
   padding: 80px 30px;
+  @media (min-width: 992px) {
+    height: 90dvh;
+  }
   @media (max-width: 960px) {
     padding: 66px 16px;
   }
